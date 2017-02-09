b13b47ac1126d335a2a22b39098e9c9fe6767435
Saravjeet 'Aman' Singh <saravjeetamansingh@gmail.com>
2017-02-09 06:20:50 +0530

    improve scraper

--- examples/c/hn.c
+++ examples/c/hn.c
@@ -19,6 +19,9 @@
 #include "xp.h"
 #include "debug.h"
 
+#define START_URL "https://news.ycombinator.com"
+
+/* utils */
 int index_at_occurance(char *str, char c, int o)
 {
     int i;
@@ -42,21 +45,140 @@ bool cleanup()
     return fetch_cleanup() && xp_cleanup();
 }
 
+void process(GHashTable *obj)
+{
+    char *keys[] = {"name", "url", "user", "comments"};
+    int key_len = sizeof(keys)/sizeof(keys[0]);
+    int i;
+
+    printf("{\n");
+    for ( i = 0; i < key_len; ++i ) {
+        printf("\t\"%s\": \"%s\"\n", keys[i], (char *) g_hash_table_lookup(obj, keys[i]));
+    }
+    printf("}\n");
+}
+
+gboolean hash_table_clear(gpointer key, gpointer value, gpointer udata)
+{
+    if ( value ) free(value);
+    return true;
+}
+
 char *scrape(char *body)
 {
     struct xp_object *doc = xp_doc_new(body);
-    struct xp_list *titles = xp_exec(doc, "//a[@class='storylink']");
-    int i;
-    for ( i = 0; i < titles->len; ++i ) {
-        xmlNodePtr car;
-        car = titles->nodes[i]->children;
-        while(car) {
-            if ( car->type == XML_TEXT_NODE ) {
-                printf("%s\n", car->content);
-            }
-            car = car->next;
-        }
+    struct xp_list *storylinks = xp_exec(doc, "//table[@class='itemlist']//td[@class='title']/a[@class='storylink']");
+    struct xp_list *subtexts = xp_exec(doc, "//table[@class='itemlist']//td[@class='subtext']");
+
+    int min_length;
+    if ( storylinks->len < subtexts->len ) {
+        min_length = storylinks->len;
+    } else {
+        min_length = subtexts->len;
     }
+
+    int i;
+    GHashTable *obj = g_hash_table_new(g_str_hash, g_str_equal);
+    for ( i = 0; i < min_length; ++i ) {
+
+
+        xmlNodePtr head = storylinks->nodes[i];
+        xmlNodePtr opt = subtexts->nodes[i];
+        
+        xmlAttrPtr attr_tmp;
+        xmlNodePtr tmp; 
+
+        // extract the text of the post
+        tmp = head->children;
+        while ( tmp ) {
+            if ( tmp->type == XML_TEXT_NODE ) {
+                g_hash_table_insert(obj, "name", strdup((const char *)tmp->content));
+            }
+            tmp = tmp->next;
+        }
+
+        // extract the link
+        attr_tmp = head->properties;
+        while(attr_tmp) {
+            if ( !strcmp((const char *)attr_tmp->name, "href") ) {
+                tmp = attr_tmp->children;
+                while(tmp) {
+                    if ( tmp->type == XML_TEXT_NODE ) {
+                        // make sure it leads to a valid domain
+                        if ( !strncmp((const char *)tmp->content, "http", 4) ) {
+                            g_hash_table_insert(obj, "url", strdup((const char *)tmp->content));
+                        } else {
+                        // ... else it's probably a self post
+                            char *link = g_strjoin(NULL, START_URL, "/", tmp->content, NULL);
+                            g_hash_table_insert(obj, "url", link);
+                        }
+                    }
+                    tmp = tmp->next;
+                }
+            }
+            attr_tmp = attr_tmp->next;
+        }
+
+        // user's name
+        tmp = opt->children;
+        while(tmp) {
+            if ( tmp->type == XML_ELEMENT_NODE && !strcmp((const char *)tmp->name, "a") ) {
+                attr_tmp = tmp->properties;
+                while(attr_tmp) {
+                    if ( !strcmp((const char *)attr_tmp->name, "class") ) {
+                        xmlNodePtr class = attr_tmp->children;
+                        while(class) {
+                            if ( class->type == XML_TEXT_NODE && !strcmp((const char *)class->content, "hnuser") ) {
+                                xmlNodePtr val = tmp->children;
+                                while(val) {
+                                    if ( val->type == XML_TEXT_NODE ) {
+                                        g_hash_table_insert(obj, "user", strdup((const char *)val->content));
+                                    }
+                                    val = val->next;
+                                }
+                            }
+                            class = class->next;
+                        }
+                    }
+                    attr_tmp = attr_tmp->next;
+                }
+            }
+            tmp = tmp->next;
+        }
+
+        // the link to the comments is the last node in the element
+        tmp = opt->children;
+        while(tmp) {
+            if ( !strcmp((const char *)tmp->name, "a") ) {
+                attr_tmp = tmp->properties;
+                while(attr_tmp) {
+                    if ( !strcmp((const char *)attr_tmp->name, "href") ) {
+                        xmlNodePtr val = attr_tmp->children;
+                        while(val) {
+                            if ( val->type == XML_TEXT_NODE && !strncmp((const char *)val->content, "item", 4) ) {
+                                char *link = g_strjoin(NULL, START_URL, "/", val->content, NULL);
+                                g_hash_table_insert(obj, "comments", link);
+                            }
+                            val = val->next;
+                        }
+                    }
+                    attr_tmp = attr_tmp->next;
+                }
+            }
+            tmp = tmp->next;
+        }
+        
+
+        process(obj);
+
+        // empty the object
+        g_hash_table_foreach_remove(obj, hash_table_clear, NULL);
+
+    }
+    // free up resources
+    g_hash_table_destroy(obj);
+    xp_list_free(storylinks);
+    xp_list_free(subtexts);
 
     // find the next hypertext reference, if any.
     char *next_href = NULL;
@@ -74,10 +196,8 @@ char *scrape(char *body)
     }
 
 
-    xp_doc_free(doc);
-    xp_list_free(titles);
     xp_list_free(href);
-
+    xp_doc_free(doc);
 
     return next_href;
 }
